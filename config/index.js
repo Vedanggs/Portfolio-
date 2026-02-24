717c6f2f1b156dc10bad958cb8851295c99c3ea1
Vedanggs <vedangrekha@gmail.com>
2026-02-24 17:36:18 +0530

    Initial Commit: Portfolio

---
+++ config/index.js
@@ -0,0 +1,9 @@
+
+
+// EMAILJS Config Data
+
+export const EMAILJS_TEMPLATE_ID = "template_hu6w805"
+
+export const EMAILJS_SERVICE_ID = "service_5r0vlyt"
+
+export const EMAILJS_PUBLIC_KEY = "MgSfBGefXk61b6ydr"
